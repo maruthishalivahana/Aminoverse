e2d4acd38ef84aa3c3baf1c68a4aab87bbde1954
maruthishalivahana <maruthikummari553@gmail.com>
2025-05-22 17:22:25 +0530

    routing

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import Userinput from "./components/Userinput.jsx";
 import './App.css';
@@ -9,16 +9,15 @@ import Login from './components/Login.jsx';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* This is the default route shown when user opens the site */}
-        <Route path="/" element={<Landingpage />} />
 
-        {/* This is the login route */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Userinput />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      {/* This is the default route shown when user opens the site */}
+      <Route path="/" element={<Landingpage />} />
+
+      {/* This is the login route */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/home" element={<Userinput />} />
+    </Routes>
   );
 }
 
